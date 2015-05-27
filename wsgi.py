92ff372da56a2e83e99c05a0492c05f905072aad
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-05-27 19:09:03 +0000

    wsgi: add wsgi.py entrypoint

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+from werkzeug.serving import run_simple
+from werkzeug.wsgi import DispatcherMiddleware
+from solr import app
+
+application = app.create_app()
+
+if __name__ == "__main__":
+    run_simple('0.0.0.0', 4000, application, use_reloader=False, use_debugger=True)
+
