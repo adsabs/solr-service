8ccbc2f6fc80cba4137f88fe566ecb96fa0bf7e8
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-07-24 18:47:04 +0000

    Config: Use flask-consulate to load config

--- solr/app.py
+++ solr/app.py
@@ -2,17 +2,19 @@ from flask import Flask, make_response, jsonify
 from views import StatusView, Tvrh, Search, Qtree, BigQuery
 from flask.ext.restful import Api
 from flask.ext.discoverer import Discoverer
+from flask.ext.consulate import Consul, ConsulConnectionError
 
 
 def create_app():
+    """
+    Application factory
+    :return configured flask.Flask application instance
+    """
     app = Flask(__name__, static_folder=None)
 
     app.url_map.strict_slashes = False
-    app.config.from_pyfile('config.py')
-    try:
-        app.config.from_pyfile('local_config.py')
-    except IOError:
-        pass
+    Consul(app)  # load_config expects consul to be registered
+    load_config(app)
 
     api = Api(app)
 
@@ -43,6 +45,29 @@ def create_app():
 
     Discoverer(app)
     return app
+
+
+def load_config(app):
+    """
+    Loads configuration in the following order:
+        1. config.py
+        2. local_config.py (ignore failures)
+        3. consul (ignore failures)
+    :param app: flask.Flask application instance
+    :return: None
+    """
+
+    app.config.from_pyfile('config.py')
+
+    try:
+        app.config.from_pyfile('local_config.py')
+    except IOError:
+        app.logger.warning("Could not load local_config.py")
+    try:
+        app.extensions['consul'].apply_remote_config()
+    except ConsulConnectionError, e:
+        app.logger.warning("Could not apply config from consul: {}".format(e))
+
 
 if __name__ == "__main__":
     app = create_app()
