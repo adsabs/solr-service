f29da63f722cd605bd181845cf7366a8db4b756d
Jean-Claude Paquin <jean-claude.paquin@cfa.harvard.edu>
2025-12-02 13:53:27 -0500

    Set UID using new header name

--- solr/views.py
+++ solr/views.py
@@ -229,6 +229,8 @@ class SolrInterface(Resource):
 
         if not user_id:
             user_id = request.headers.get('X-Adsws-Uid', 'default')
+            if user_id == 'default':
+                user_id = request.headers.get('X-api-uid', 'default')
 
         headers = {}
         _h = request.headers.get('Content-Type', 'application/x-www-form-urlencoded')
