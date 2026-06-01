abadf81a96ac329650ff607e9b48f717f9f712d6
Jean-Claude Paquin <jean-claude.paquin@cfa.harvard.edu>
2026-06-01 16:59:27 -0400

    Change time columns to appropriate types

--- alembic/versions/59ce6fb5526_create_query_logging_table.py
+++ alembic/versions/59ce6fb5526_create_query_logging_table.py
@@ -22,10 +22,10 @@ from sqlalchemy import String, Integer, Index
 def upgrade():
     op.create_table('query_log',
         sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('timestamp', sa.Integer, nullable=False),
+        sa.Column('timestamp', sa.DateTime, nullable=False),
         sa.Column('query', sa.Text, nullable=False),
         sa.Column('allocated_bytes', sa.Integer, nullable=False),
-        sa.Column('request_duration', sa.Integer, nullable=False)
+        sa.Column('request_duration', sa.Interval, nullable=False)
     )
 
 def downgrade():
--- solr/models.py
+++ solr/models.py
@@ -5,7 +5,7 @@
 
     Models for the users (users) of AdsWS
 """
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, DateTime, Interval
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -30,7 +30,7 @@ class Limits(Base):
 class QueryLog(Base):
     __tablename__ = 'query_log'
     id = Column(Integer, primary_key=True)
-    timestamp = Column("timestamp", Integer, nullable=False)
+    timestamp = Column("timestamp", DateTime, nullable=False)
     query = Column("query", Text, nullable=False)
     allocated_bytes = Column("allocated_bytes", Integer, nullable=False)
-    request_duration = Column("request_duration", Integer, nullable=False)
+    request_duration = Column("request_duration", Interval, nullable=False)
