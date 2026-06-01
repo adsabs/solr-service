01d720ea974668ddbdc93762063c8520a219b7c5
Jean-Claude Paquin <jean-claude.paquin@cfa.harvard.edu>
2026-06-01 16:47:56 -0400

    Add Alembic version with query_log table

---
+++ alembic/versions/59ce6fb5526_create_query_logging_table.py
@@ -0,0 +1,31 @@
+"""Create query logging table
+
+Revision ID: 59ce6fb5526
+Revises: 51f3b3b5cd5d
+Create Date: 2026-05-22 15:22:17.468189
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '59ce6fb5526'
+down_revision = '51f3b3b5cd5d'
+
+from alembic import op
+import sqlalchemy as sa
+
+import datetime
+
+from sqlalchemy.sql import table, column
+from sqlalchemy import String, Integer, Index
+
+
+def upgrade():
+    op.create_table('query_log',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('query', sa.Text, nullable=False),
+        sa.Column('allocated_bytes', sa.Integer, nullable=False),
+        sa.Column('request_duration', sa.Integer, nullable=False)
+    )
+
+def downgrade():
+    op.drop_table('query_log')
